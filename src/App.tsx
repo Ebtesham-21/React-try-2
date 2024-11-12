d3aa8bba17a71f572dfaf26ab48587a4deecd3a1
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-11-12 12:30:17 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup/ListGroup";
+import ButtonBoot from "./components/ButtonBoot";
+import Button from "./components/Button";
+import { useState } from "react";
+
+function App() {
+  let items = ["Dhaka", "kg", "Sylhet", "Habiganj"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  // const [showAlert, setShowAlert] = useState(false);
+
+  // const handleShowAlert = () => {
+  //   setShowAlert((prevShowAlert) => !prevShowAlert);
+  // };
+  return (
+    // <div>
+    //   {showAlert && (
+    //     <Alert>
+    //       Hi <span>Test</span>
+    //     </Alert>
+    //   )}
+    //   {/* <ButtonBoot /> */}
+    //   <Button color="primary" onClick={handleShowAlert}>
+    //     Click
+    //   </Button>
+    // </div>
+    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+  );
+}
+
+export default App;
